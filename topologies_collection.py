31174afa4df04191828136da3bb2fcc7447c14a5
Goshmar <62976616+Goshmar@users.noreply.github.com>
2023-10-17 17:58:37 +0300

    Update and rename OurTopologies.py to topologies_collection.py

--- topologies_collection.py
+++ topologies_collection.py
@@ -4,16 +4,21 @@ import networkx as nx
 import random
 from topologies import AveragingScheme
 
-
-class SlowChangingGraph():
+class SlowChangingGraph:
 
     def __init__(self, num_workers, k, seed=42, init_p=0.5):
-        super().__init__()
+        """
+        Initialize a SlowChangingGraph instance.
+        """
         self.num_workers = num_workers
         self.k = k
         self.init_p = init_p
         self.seed = seed
         self.iteration = 0
+        self._initialize_graph()
+
+    def _initialize_graph(self):
+        # Initialize a connected graph
         random_iter = 0
         while True:
             graph = nx.erdos_renyi_graph(self.num_workers, self.init_p, seed=self.seed + random_iter)
@@ -23,9 +28,12 @@ class SlowChangingGraph():
             random_iter += 1
 
     def next_step(self):
+        """
+        Move to the next step by changing edges in the graph.
+        """
         random_iteration = 0
         while True:
-            edges_to_change = set([])
+            edges_to_change = set()
             new_graph = self.graph.copy()
             while len(edges_to_change) < self.k:
                 random.seed(self.seed + random_iteration + 100 * self.iteration)
@@ -38,7 +46,7 @@ class SlowChangingGraph():
                     new_graph.remove_edge(node1, node2)
                 else:
                     new_graph.add_edge(node1, node2)
-                edges_to_change = edges_to_change.union(set([(node1, node2)]))
+                edges_to_change.add((node1, node2))
                 random_iteration += 1
 
             if nx.is_connected(new_graph):
@@ -49,26 +57,34 @@ class SlowChangingGraph():
         self.iteration += 1
 
     def get_w(self):
+        """
+        Get the adjacency matrix for the current graph.
+        """
         G = nx.to_numpy_array(self.graph)
-        G = torch.tensor(G)
-        num_neighbors = G.sum(1, True)
+        G = torch.tensor(G, dtype=torch.float32)
+        num_neighbors = G.sum(1, keepdim=True)
         normalization = 1 / (torch.max(num_neighbors, num_neighbors.T) + 1)
         G = normalization * G
         G += torch.diag(1 - G.sum(1))
         return G
 
 
-class SlowChangingRing():
+class SlowChangingRing:
 
     def __init__(self, num_workers, k, seed=42):
-        super().__init__()
+        """
+        Initialize a SlowChangingRing instance.
+        """
         self.num_workers = num_workers
         self.k = k
         self.seed = seed
 
     def w(self, t=0, params=None):
+        """
+        Generate the adjacency matrix for the ring graph.
+        """
         graph = nx.cycle_graph(self.num_workers)
-        edges_to_change = set([])
+        edges_to_change = set()
         random_iteration = 0
         while len(edges_to_change) < self.k:
             random.seed(self.seed + 100 * t + random_iteration)
@@ -78,29 +94,34 @@ class SlowChangingRing():
                 random_iteration += 1
                 continue
             graph.add_edge(node1, node2)
-            edges_to_change = edges_to_change.union(set([(node1, node2)]))
+            edges_to_change.add((node1, node2))
             random_iteration += 1
 
         G = nx.to_numpy_array(graph)
-        G = torch.tensor(G)
-        num_neighbors = G.sum(1, True)
+        G = torch.tensor(G, dtype=torch.float32)
+        num_neighbors = G.sum(1, keepdim=True)
         normalization = 1 / (torch.max(num_neighbors, num_neighbors.T) + 1)
         G = normalization * G
         G += torch.diag(1 - G.sum(1))
         return G
 
 
-class SlowChangingStar():
+class SlowChangingStar:
 
     def __init__(self, num_workers, k, seed=42):
-        super().__init__()
+        """
+        Initialize a SlowChangingStar instance.
+        """
         self.num_workers = num_workers
         self.k = k
         self.seed = seed
 
     def w(self, t=0, params=None):
+        """
+        Generate the adjacency matrix for the star graph.
+        """
         graph = nx.star_graph(self.num_workers)
-        edges_to_change = set([])
+        edges_to_change = set()
         random_iteration = 0
         while len(edges_to_change) < self.k:
             random.seed(self.seed + 100 * t + random_iteration)
@@ -110,26 +131,28 @@ class SlowChangingStar():
                 random_iteration += 1
                 continue
             graph.add_edge(node1, node2)
-            edges_to_change = edges_to_change.union(set([(node1, node2)]))
+            edges_to_change.add((node1, node2))
             random_iteration += 1
 
         G = nx.to_numpy_array(graph)
-        G = torch.tensor(G)
-        num_neighbors = G.sum(1, True)
+        G = torch.tensor(G, dtype=torch.float32)
+        num_neighbors = G.sum(1, keepdim=True)
         normalization = 1 / (torch.max(num_neighbors, num_neighbors.T) + 1)
         G = normalization * G
         G += torch.diag(1 - G.sum(1))
         return G
 
-
 class TimeVaringErdos(AveragingScheme):
 
     def __init__(self, n, p, seed=None):
-        self.period = 100
+        """
+        Initialize a TimeVaryingErdos instance.
+        """
         super().__init__()
         self.n = n
         self.p = p
         self.seed = seed
+        self.period = 100
 
     def w(self, t=0, params=None):
         if self.seed is not None:
@@ -145,7 +168,7 @@ class TimeVaringErdos(AveragingScheme):
 
         G = nx.to_numpy_array(G)
         G = torch.tensor(G, dtype=torch.float32)
-        num_neighbors = G.sum(1, True)
+        num_neighbors = G.sum(1, keepdim=True)
         normalization = 1 / (torch.max(num_neighbors, num_neighbors.T) + 1)
         G = normalization * G
         G += torch.diag(1 - G.sum(1))
@@ -153,56 +176,83 @@ class TimeVaringErdos(AveragingScheme):
 
 
 class Erdos(AveragingScheme):
+
     def __init__(self, n, p):
+        """
+        Initialize an Erdos instance.
+        """
+        super().__init__()
         self.n = n
         self.p = p
+        self.G = self._generate_graph()
+
+    def _generate_graph(self):
         G = nx.erdos_renyi_graph(self.n, self.p)
+        while not nx.is_connected(G):
+            G = nx.erdos_renyi_graph(self.n, self.p)
         G = nx.to_numpy_array(G)
         G = torch.tensor(G, dtype=torch.float32)
-        num_neighbors = G.sum(1, True)
+        num_neighbors = G.sum(1, keepdim=True)
         normalization = 1 / (torch.max(num_neighbors, num_neighbors.T) + 1)
         G = normalization * G
         G += torch.diag(1 - G.sum(1))
-        self.G = G
+        return G
 
     def w(self, t=0, params=None):
         return self.G
 
 
-class Ring():
+class Ring:
+
     def __init__(self, n):
+        """
+        Initialize a Ring instance.
+        """
         self.n = n
+        self.G = self._generate_graph()
+
+    def _generate_graph(self):
         G = nx.cycle_graph(self.n)
         G = nx.to_numpy_array(G)
         G = torch.tensor(G, dtype=torch.float32)
-        num_neighbors = G.sum(1, True)
+        num_neighbors = G.sum(1, keepdim=True)
         normalization = 1 / (torch.max(num_neighbors, num_neighbors.T) + 1)
         G = normalization * G
         G += torch.diag(1 - G.sum(1))
-        self.G = G
+        return G
 
     def w(self, t=0, params=None):
         return self.G
 
 
-class Star():
+class Star:
+
     def __init__(self, n):
+        """
+        Initialize a Star instance.
+        """
         self.n = n
+        self.G = self._generate_graph()
+
+    def _generate_graph(self):
         G = nx.star_graph(self.n)
         G = nx.to_numpy_array(G)
         G = torch.tensor(G, dtype=torch.float32)
-        num_neighbors = G.sum(1, True)
+        num_neighbors = G.sum(1, keepdim=True)
         normalization = 1 / (torch.max(num_neighbors, num_neighbors.T) + 1)
         G = normalization * G
         G += torch.diag(1 - G.sum(1))
-        self.G = G
+        return G
 
     def w(self, t=0, params=None):
         return self.G
 
+class CycleChangingGraph:
 
-class CycleChangingGraph():
     def __init__(self, n, topologies):
+        """
+        Initialize a CycleChangingGraph instance.
+        """
         self.n = n
         self.topologies = topologies
 
